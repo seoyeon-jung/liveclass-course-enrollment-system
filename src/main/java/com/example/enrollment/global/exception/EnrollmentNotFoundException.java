71558f724af12debee78dec13601898a08b5a683
seoyeon-jung <jungluchia1204@gmail.com>
2026-04-06 16:44:24 +0900

    feat: EnrollmentNotFoundException 추가 및 전역 예외 핸들러 보완(#3)

--- src/main/java/com/example/enrollment/global/exception/EnrollmentNotFoundException.java
+++ src/main/java/com/example/enrollment/global/exception/EnrollmentNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.enrollment.global.exception;
 
-public class EnrollmentNotFoundException {
+public class EnrollmentNotFoundException extends RuntimeException {
+    public EnrollmentNotFoundException(Long enrollmentId) {
+        super("수강 신청을 찾을 수 없습니다. id=" + enrollmentId);
+    }
 }
