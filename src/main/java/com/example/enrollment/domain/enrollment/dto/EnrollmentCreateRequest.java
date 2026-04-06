8e096899f6d989e58de478c30a4f76667bad9abf
seoyeon-jung <jungluchia1204@gmail.com>
2026-04-06 16:43:50 +0900

    feat: Enrollment 요청/응답 DTO 추가(#3)

--- src/main/java/com/example/enrollment/domain/enrollment/dto/EnrollmentCreateRequest.java
+++ src/main/java/com/example/enrollment/domain/enrollment/dto/EnrollmentCreateRequest.java
@@ -1,4 +1,12 @@
 package com.example.enrollment.domain.enrollment.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
 public class EnrollmentCreateRequest {
+
+    @NotNull(message = "강의 ID는 필수입니다.")
+    private Long courseId;
+
 }
