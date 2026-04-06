d7265fd8f380e32c681aeddf7075776ea5727644
seoyeon-jung <jungluchia1204@gmail.com>
2026-04-06 15:58:39 +0900

    feat: Course 요청/응답 DTO 추가(#2)

--- src/main/java/com/example/enrollment/domain/course/dto/CourseStatusUpdateRequest.java
+++ src/main/java/com/example/enrollment/domain/course/dto/CourseStatusUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.example.enrollment.domain.course.dto;
 
+import com.example.enrollment.domain.course.entity.CourseStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
 public class CourseStatusUpdateRequest {
+
+    @NotNull(message = "변경할 상태는 필수입니다.")
+    private CourseStatus status;
+
 }
