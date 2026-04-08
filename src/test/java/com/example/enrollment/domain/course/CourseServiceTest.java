0f02466995221868d9e37e228ae2f630c0236d0d
seoyeon-jung <jungluchia1204@gmail.com>
2026-04-08 11:56:40 +0900

    feat: DTO test용 생성자 추가 (#6)

---
+++ src/test/java/com/example/enrollment/domain/course/CourseServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.enrollment.domain.course;
+
+public class CourseServiceTest {
+}
---
+++ src/test/java/com/example/enrollment/domain/enrollment/EnrollmentServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.enrollment.domain.enrollment;
+
+public class EnrollmentServiceTest {
+}
